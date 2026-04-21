9dc065622fd8784ed8044ae57dd162965a1b517a
DevDyna <andrearisorti2003@gmail.com>
2026-04-21 23:43:06 +0200

    Update DataLang.java

--- src/main/java/com/synergy/machines/datagen/client/DataLang.java
+++ src/main/java/com/synergy/machines/datagen/client/DataLang.java
@@ -3,7 +3,10 @@ package com.synergy.machines.datagen.client;
 import static com.devdyna.cakesticklib.api.datagen.LangUtils.*;
 import static com.synergy.machines.Main.MODULE_ID;
 
+import com.synergy.machines.init.Material;
+
 import net.minecraft.data.PackOutput;
+import net.minecraft.world.level.block.Block;
 import net.neoforged.neoforge.common.data.LanguageProvider;
 import net.neoforged.neoforge.registries.DeferredHolder;
 
@@ -16,6 +19,8 @@ public class DataLang extends LanguageProvider {
 
         @Override
         protected void addTranslations() {
+
+                Material.getAllMachineTypes().forEach(m -> addBlock(m.block(), named(m.block(), MODULE_ID)));
 
         }
 
