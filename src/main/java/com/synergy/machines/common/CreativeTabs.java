083edce0e542d2f3785ab4ce66518972a9676f53
DevDyna <andrearisorti2003@gmail.com>
2026-04-22 15:24:25 +0200

    Update CreativeTabs.java

--- src/main/java/com/synergy/machines/common/CreativeTabs.java
+++ src/main/java/com/synergy/machines/common/CreativeTabs.java
@@ -1,6 +1,8 @@
 package com.synergy.machines.common;
 
 import com.synergy.machines.init.Material;
+import com.synergy.machines.init.types.zBlocks;
+import com.synergy.machines.init.types.zItems;
 
 import net.minecraft.world.item.CreativeModeTabs;
 import net.neoforged.bus.api.SubscribeEvent;
@@ -10,11 +12,21 @@ public class CreativeTabs {
 
     @SubscribeEvent
     public static void register(BuildCreativeModeTabContentsEvent event) {
-        if (event.getTabKey() == CreativeModeTabs.REDSTONE_BLOCKS){
+        if (event.getTabKey() == CreativeModeTabs.REDSTONE_BLOCKS) {
 
-            Material.getAllMachineTypes().forEach(m->event.accept(m.item().get()));
+            Material.getAllMachineTypes().forEach(m -> event.accept(m.item().get()));
 
-
+            event.accept(zBlocks.SOLAR_PANEL_DAY.get());
+            event.accept(zBlocks.SOLAR_PANEL_NIGHT.get());
         }
+
+        if (event.getTabKey() == CreativeModeTabs.TOOLS_AND_UTILITIES) {
+
+            event.accept(zItems.SPEED_UPGRADE.get().set(20, 125, 0, 0));
+            event.accept(zItems.ENERGY_UPGRADE.get().set(0, -50, 0, 0));
+            event.accept(zItems.LUCK_UPGRADE.get().set(0, 150, 15, 0));
+            event.accept(zItems.FLUID_UPGRADE.get().set(0, 150, 0, -20));
+        }
+
     }
 }
