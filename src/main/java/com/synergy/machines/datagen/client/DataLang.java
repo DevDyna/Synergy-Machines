7240a0b09466fa734522b00def75e839fc74cde4
DevDyna <andrearisorti2003@gmail.com>
2026-04-22 15:24:22 +0200

    Update DataLang.java

--- src/main/java/com/synergy/machines/datagen/client/DataLang.java
+++ src/main/java/com/synergy/machines/datagen/client/DataLang.java
@@ -4,6 +4,7 @@ import static com.devdyna.cakesticklib.api.datagen.LangUtils.*;
 import static com.synergy.machines.Main.MODULE_ID;
 
 import com.synergy.machines.init.Material;
+import com.synergy.machines.init.types.*;
 
 import net.minecraft.data.PackOutput;
 import net.minecraft.world.level.block.Block;
@@ -21,6 +22,21 @@ public class DataLang extends LanguageProvider {
         protected void addTranslations() {
 
                 Material.getAllMachineTypes().forEach(m -> addBlock(m.block(), named(m.block(), MODULE_ID)));
+
+                addBlock(zBlocks.SOLAR_PANEL_DAY, "Solar Panel (Daytime)");
+                addBlock(zBlocks.SOLAR_PANEL_NIGHT, "Solar Panel (Nighttime)");
+
+                addItem(zItems.ENERGY_UPGRADE, named(zItems.ENERGY_UPGRADE, MODULE_ID));
+                addItem(zItems.SPEED_UPGRADE, named(zItems.SPEED_UPGRADE, MODULE_ID));
+                addItem(zItems.FLUID_UPGRADE, named(zItems.FLUID_UPGRADE, MODULE_ID));
+                addItem(zItems.LUCK_UPGRADE, named(zItems.LUCK_UPGRADE, MODULE_ID));
+
+                add(MODULE_ID + ".screen.upgrades", "Supported Upgrades:");
+
+                add(MODULE_ID + ".screen.modifier.energy", TIP_COLOR + "Energy Modifier §7[§f§a%s§7]");
+                add(MODULE_ID + ".screen.modifier.speed", TIP_COLOR + "Speed Modifier §7[§f§a%s§7]");
+                add(MODULE_ID + ".screen.modifier.luck", TIP_COLOR + "Luck Modifier §7[§f§a%s§7]");
+                add(MODULE_ID + ".screen.modifier.fluid", TIP_COLOR + "Fluid Modifier §7[§f§a%s§7]");
 
         }
 
