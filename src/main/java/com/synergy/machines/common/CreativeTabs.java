42ad7fba3b0f0e568fe4f780ebbbb173eebfec5e
DevDyna <andrearisorti2003@gmail.com>
2026-04-26 14:22:51 +0200

    updated cakesticklib

--- src/main/java/com/synergy/machines/common/CreativeTabs.java
+++ src/main/java/com/synergy/machines/common/CreativeTabs.java
@@ -2,8 +2,6 @@ package com.synergy.machines.common;
 
 import com.synergy.machines.init.Material;
 import com.synergy.machines.init.types.zBlocks;
-import com.synergy.machines.init.types.zItems;
-
 import net.minecraft.world.item.CreativeModeTabs;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
@@ -18,14 +16,6 @@ public class CreativeTabs {
 
             event.accept(zBlocks.SOLAR_PANEL_DAY.get());
             event.accept(zBlocks.SOLAR_PANEL_NIGHT.get());
-        }
-
-        if (event.getTabKey() == CreativeModeTabs.TOOLS_AND_UTILITIES) {
-
-            event.accept(zItems.SPEED_UPGRADE.get().set(20, 125, 0, 0));
-            event.accept(zItems.ENERGY_UPGRADE.get().set(0, -50, 0, 0));
-            event.accept(zItems.LUCK_UPGRADE.get().set(0, 150, 15, 0));
-            event.accept(zItems.FLUID_UPGRADE.get().set(0, 150, 0, -20));
         }
 
     }
