f73d8ea1840188cb27adb77d7d37a0dc5c7478f6
DevDyna <andrearisorti2003@gmail.com>
2026-04-26 14:23:17 +0200

    Update BaseMachineBE.java

--- src/main/java/com/synergy/machines/api/machine/BaseMachineBE.java
+++ src/main/java/com/synergy/machines/api/machine/BaseMachineBE.java
@@ -1,22 +1,17 @@
 package com.synergy.machines.api.machine;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import com.devdyna.cakesticklib.api.aspect.logic.*;
 import com.devdyna.cakesticklib.api.aspect.templates.MachineBE;
 import com.devdyna.cakesticklib.api.recipe.recipeType.BaseRecipeType;
-import com.devdyna.cakesticklib.api.utils.UpgradeComponents;
-import com.devdyna.cakesticklib.api.utils.UpgradeComponents.UpgradeType;
 import com.devdyna.cakesticklib.setup.registry.zLibrary.zComponents;
 import com.devdyna.cakesticklib.setup.registry.zLibrary.zHandlers;
 import com.mojang.logging.LogUtils;
-import com.synergy.machines.Common;
 import com.synergy.machines.api.MachineType;
 import com.synergy.machines.api.RecipeRegister;
 import com.synergy.machines.api.machine.recipe.BaseMachineRecipeType;
-import com.synergy.machines.init.builders.IndustrialUpgrade;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.inventory.ContainerData;
@@ -31,6 +26,7 @@ import net.minecraft.world.level.storage.ValueOutput;
 import net.neoforged.neoforge.fluids.FluidStack;
 import net.neoforged.neoforge.transfer.energy.EnergyHandler;
 import net.neoforged.neoforge.transfer.item.ItemResource;
+import net.neoforged.neoforge.transfer.item.ItemStackResourceHandler;
 import net.neoforged.neoforge.transfer.item.ItemStacksResourceHandler;
 import net.neoforged.neoforge.transfer.transaction.Transaction;
 
@@ -56,7 +52,7 @@ import net.neoforged.neoforge.transfer.transaction.Transaction;
  * credit: @DevDyna
  */
 @SuppressWarnings("null")
-public abstract class BaseMachineBE extends MachineBE implements MachineItemAutomation, EnergyBlock {
+public abstract class BaseMachineBE extends MachineBE implements MachineItemAutomation, EnergyBlock , UpgradeInstallable{
 
     public static final int DEFAULT_FE_COST = 500;
     public static final int DEFAULT_TICK_DURATION = 100;
@@ -374,79 +370,84 @@ public abstract class BaseMachineBE extends MachineBE implements MachineItemAuto
             stack.grow(slotStack.getCount());
     }
 
-    public List<Integer> getUpgradeIndexs() {
+    public List<Integer> getUpgradeSlots() {
         return List.of(SLOT_UPGRADE_1, SLOT_UPGRADE_2, SLOT_UPGRADE_3, SLOT_UPGRADE_4).stream()
                 .filter(i -> i < getSlots())
                 .toList();
     }
 
-    public List<ItemStack> getUpgradeInstalled() {
-        return getUpgradeIndexs().stream()
-                .map(this::getStackInSlot)
-                .filter(i -> i.getItem() instanceof IndustrialUpgrade)
-                .filter(i -> i.get(zComponents.UPGRADE_COMPONENTS) != null)
-                .toList();
+    // public List<ItemStack> getUpgradeInstalled() {
+    //     return getUpgradeIndexs().stream()
+    //             .map(this::getStackInSlot)
+    //             .filter(i -> i.getItem() instanceof IndustrialUpgrade)
+    //             .filter(i -> i.get(zComponents.UPGRADE_COMPONENTS) != null)
+    //             .toList();
+    // }
+
+    // public List<Integer> getValues(UpgradeType type) {
+    //     List<ItemStack> upgrades = getUpgradeInstalled().stream()
+    //             .filter(i -> UpgradeComponents.has(i, type))
+    //             .toList();
+
+    //     List<Integer> validSlots = new ArrayList<>();
+    //     int maxRoll = getTypeLimiter(type);
+
+    //     for (int i = 0; i < upgrades.size() && validSlots.size() < maxRoll; i++)
+    //         for (int j = 0; j < upgrades.get(i).getCount() && validSlots.size() < maxRoll; j++)
+    //             validSlots.add(UpgradeComponents.get(upgrades.get(i), type));
+
+    //     return validSlots;
+    // }
+
+    // public int getTypeLimiter(UpgradeType type) {
+    //     if (type.equals(UpgradeType.SPEED))
+    //         return Common.MACHINE_MAX_SPEED_UPGRADES_TYPE.get();
+    //     if (type.equals(UpgradeType.ENERGY))
+    //         return Common.MACHINE_MAX_ENERGY_EFFICIENCY_UPGRADES_TYPE.get();
+    //     if (type.equals(UpgradeType.LUCK))
+    //         return Common.MACHINE_MAX_LUCK_UPGRADES_TYPE.get();
+    //     if (type.equals(UpgradeType.FLUID))
+    //         return Common.MACHINE_MAX_FLUID_UPGRADES_TYPE.get();
+    //     return Integer.MAX_VALUE;
+    // }
+
+    @Override
+    public ItemStackResourceHandler getUpgradeItemStorage() {
+        return null;//TODO
     }
 
-    public List<Integer> getValues(UpgradeType type) {
-        List<ItemStack> upgrades = getUpgradeInstalled().stream()
-                .filter(i -> UpgradeComponents.has(i, type))
-                .toList();
+    // public int calculateMaxProgress(int base) {
+    //     var upgrades = getValues(UpgradeType.SPEED);
+    //     float sum = upgrades == null ? 0 : upgrades.stream().mapToInt(Integer::intValue).sum();
+    //     return Common.MACHINE_MAX_SPEED_UPGRADES_TYPE.get() == 0 ? base
+    //             : Math.max(Common.MACHINE_MINIMAL_TICK_DELAY.get(),
+    //                     (int) (base - (base * sum / 100)));
+    // }
 
-        List<Integer> validSlots = new ArrayList<>();
-        int maxRoll = getTypeLimiter(type);
+    // private int calculateFEUsage(int base) {
+    //     var upgrades = getValues(UpgradeType.ENERGY);
+    //     float sum = upgrades == null ? 0 : upgrades.stream().mapToInt(Integer::intValue).sum();
+    //     return Common.MACHINE_MAX_ENERGY_EFFICIENCY_UPGRADES_TYPE.get() == 0 ? base
+    //             : Math.max(Common.MACHINE_MINIMAL_FE_COST.get(),
+    //                     (int) (base + (base * sum / 100)));
+    // }
 
-        for (int i = 0; i < upgrades.size() && validSlots.size() < maxRoll; i++)
-            for (int j = 0; j < upgrades.get(i).getCount() && validSlots.size() < maxRoll; j++)
-                validSlots.add(UpgradeComponents.get(upgrades.get(i), type));
+    // public int calculateMBUsage(int base) {
+    //     var upgrades = getValues(UpgradeType.FLUID);
+    //     float sum = upgrades == null ? 0 : upgrades.stream().mapToInt(Integer::intValue).sum();
+    //     return Common.MACHINE_MAX_FLUID_UPGRADES_TYPE.get() == 0 ? base
+    //             : Math.max(Common.MACHINE_MINIMAL_FLUID_COST.get(),
+    //                     (int) (base + (base * sum / 100f)));
+    // }
 
-        return validSlots;
-    }
-
-    public int getTypeLimiter(UpgradeType type) {
-        if (type.equals(UpgradeType.SPEED))
-            return Common.MACHINE_MAX_SPEED_UPGRADES_TYPE.get();
-        if (type.equals(UpgradeType.ENERGY))
-            return Common.MACHINE_MAX_ENERGY_EFFICIENCY_UPGRADES_TYPE.get();
-        if (type.equals(UpgradeType.LUCK))
-            return Common.MACHINE_MAX_LUCK_UPGRADES_TYPE.get();
-        if (type.equals(UpgradeType.FLUID))
-            return Common.MACHINE_MAX_FLUID_UPGRADES_TYPE.get();
-        return Integer.MAX_VALUE;
-    }
-
-    public int calculateMaxProgress(int base) {
-        var upgrades = getValues(UpgradeType.SPEED);
-        float sum = upgrades == null ? 0 : upgrades.stream().mapToInt(Integer::intValue).sum();
-        return Common.MACHINE_MAX_SPEED_UPGRADES_TYPE.get() == 0 ? base
-                : Math.max(Common.MACHINE_MINIMAL_TICK_DELAY.get(),
-                        (int) (base - (base * sum / 100)));
-    }
-
-    private int calculateFEUsage(int base) {
-        var upgrades = getValues(UpgradeType.ENERGY);
-        float sum = upgrades == null ? 0 : upgrades.stream().mapToInt(Integer::intValue).sum();
-        return Common.MACHINE_MAX_ENERGY_EFFICIENCY_UPGRADES_TYPE.get() == 0 ? base
-                : Math.max(Common.MACHINE_MINIMAL_FE_COST.get(),
-                        (int) (base + (base * sum / 100)));
-    }
-
-    public int calculateMBUsage(int base) {
-        var upgrades = getValues(UpgradeType.FLUID);
-        float sum = upgrades == null ? 0 : upgrades.stream().mapToInt(Integer::intValue).sum();
-        return Common.MACHINE_MAX_FLUID_UPGRADES_TYPE.get() == 0 ? base
-                : Math.max(Common.MACHINE_MINIMAL_FLUID_COST.get(),
-                        (int) (base + (base * sum / 100f)));
-    }
-
-    public boolean calculateSecondarySuccess(float base) {
-        var upgrades = getValues(UpgradeType.LUCK);
-        float sum = upgrades == null ? 0 : upgrades.stream().mapToInt(Integer::intValue).sum();
-        return Common.MACHINE_MAX_LUCK_UPGRADES_TYPE.get() == 0 ? false
-                : level.getRandom().nextFloat() < Math.min(
-                        Common.MACHINE_MAXIMAL_LUCK.get(),
-                        (base + (sum / 100)));
-    }
+    // public boolean calculateSecondarySuccess(float base) {
+    //     var upgrades = getValues(UpgradeType.LUCK);
+    //     float sum = upgrades == null ? 0 : upgrades.stream().mapToInt(Integer::intValue).sum();
+    //     return Common.MACHINE_MAX_LUCK_UPGRADES_TYPE.get() == 0 ? false
+    //             : level.getRandom().nextFloat() < Math.min(
+    //                     Common.MACHINE_MAXIMAL_LUCK.get(),
+    //                     (base + (sum / 100)));
+    // }
 
     /**
      * Return <code>true</code> when success
