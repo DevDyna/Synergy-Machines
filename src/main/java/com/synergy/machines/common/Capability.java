5df94c61973ab2621832aa90533f159aa5c2f33f
DevDyna <andrearisorti2003@gmail.com>
2026-08-28 17:15:30 +0200

    Update Capability.java

--- src/main/java/com/synergy/machines/common/Capability.java
+++ src/main/java/com/synergy/machines/common/Capability.java
@@ -1,21 +1,35 @@
 package com.synergy.machines.common;
 
+import java.util.List;
+
 import com.devdyna.cakesticklib.api.CapabilityUtils;
-import com.synergy.machines.init.Material;
+import com.synergy.machines.api.MachineType;
 import com.synergy.machines.init.types.zBlocks;
 import com.synergy.machines.init.types.zMachines;
 
+import net.minecraft.world.level.block.Block;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.neoforge.capabilities.RegisterCapabilitiesEvent;
+import net.neoforged.neoforge.registries.DeferredHolder;
 
 public class Capability {
 
     @SubscribeEvent
     public static void register(RegisterCapabilitiesEvent event) {
-        CapabilityUtils.registerEnergyBlock(event, zBlocks.SOLAR_PANEL_DAY.get(), zBlocks.SOLAR_PANEL_NIGHT.get());
 
-        CapabilityUtils.registerEnergyBlock(event, Material.getBlocks());
-        CapabilityUtils.registerItemBlock(event, Material.getBlocks());
+        var machines = List.of(zMachines.ALLOY_SMELTER, zMachines.CASTING_FACTORY, zMachines.COMPRESSOR,
+                                zMachines.ELECTRIC_FURNACE, zMachines.ELECTRIC_MELTER, zMachines.EXTRACTOR,
+                                zMachines.MACERATOR, zMachines.ROCK_CRUSHER)
+                                .stream()
+                                .map(MachineType::block)
+                                .map(DeferredHolder::get)
+                                .toArray(Block[]::new);
+
+
+        CapabilityUtils.registerEnergyBlock(event, zBlocks.SOLAR_PANEL.get(), zBlocks.LUNAR_PANEL.get());
+
+        CapabilityUtils.registerEnergyBlock(event, machines);
+        CapabilityUtils.registerItemBlock(event, machines);
 
         CapabilityUtils.registerFluidBlocks(event,
                 zMachines.CASTING_FACTORY.block().get(),
