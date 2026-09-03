20514dae09d8e0409a539512f2bf8799643e1aa6
DevDyna <andrearisorti2003@gmail.com>
2026-09-03 21:01:56 +0200

    Update zItems.java

--- src/main/java/com/synergy/machines/init/types/zItems.java
+++ src/main/java/com/synergy/machines/init/types/zItems.java
@@ -2,7 +2,6 @@ package com.synergy.machines.init.types;
 
 import static com.synergy.machines.Main.MODULE_ID;
 
-
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
@@ -17,8 +16,5 @@ public class zItems {
     public static final DeferredRegister.Items zBucketItems = DeferredRegister.createItems(MODULE_ID);
     public static final DeferredRegister.Items zItem = DeferredRegister.createItems(MODULE_ID);
     public static final DeferredRegister.Items zBlockItem = DeferredRegister.createItems(MODULE_ID);
-
-    
    
-
 }
