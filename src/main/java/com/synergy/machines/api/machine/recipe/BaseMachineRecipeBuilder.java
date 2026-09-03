338a0fd694516b90d49f4454019a69572f919e43
DevDyna <andrearisorti2003@gmail.com>
2026-09-03 21:14:29 +0200

    Update BaseMachineRecipeBuilder.java

--- src/main/java/com/synergy/machines/api/machine/recipe/BaseMachineRecipeBuilder.java
+++ src/main/java/com/synergy/machines/api/machine/recipe/BaseMachineRecipeBuilder.java
@@ -2,47 +2,26 @@ package com.synergy.machines.api.machine.recipe;
 
 import static com.synergy.machines.Main.MODULE_ID;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-import java.util.function.Function;
-
 import javax.annotation.Nullable;
 
 import com.devdyna.cakesticklib.api.recipe.recipeBuilder.FluidAttach.Any.SimpleFluidAttach;
 import com.devdyna.cakesticklib.api.recipe.recipeOutput.ChanceOutput;
 import com.devdyna.cakesticklib.api.utils.x;
-import com.mojang.logging.LogUtils;
 import com.synergy.machines.api.MachineType;
 import com.synergy.machines.api.machine.*;
 
-import net.minecraft.advancements.Advancement;
-import net.minecraft.advancements.AdvancementRequirements;
-import net.minecraft.advancements.AdvancementRewards;
 import net.minecraft.advancements.Criterion;
 import net.minecraft.advancements.criterion.InventoryChangeTrigger;
-import net.minecraft.core.Holder;
 import net.minecraft.core.HolderLookup.Provider;
-import net.minecraft.data.recipes.RecipeBuilder;
-import net.minecraft.data.recipes.RecipeCategory;
-import net.minecraft.data.recipes.RecipeOutput;
 import net.minecraft.resources.Identifier;
-import net.minecraft.tags.TagKey;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.ItemStackTemplate;
-import net.minecraft.world.item.crafting.Ingredient;
-import net.minecraft.world.item.crafting.Recipe;
 import net.minecraft.world.item.crafting.RecipeInput;
 import net.neoforged.neoforge.common.crafting.SizedIngredient;
-import net.neoforged.neoforge.fluids.FluidStack;
 import net.neoforged.neoforge.fluids.FluidStackTemplate;
 import net.neoforged.neoforge.fluids.crafting.SizedFluidIngredient;
-import net.neoforged.neoforge.registries.DeferredHolder;
-
 import com.devdyna.cakesticklib.api.recipe.recipeBuilder.*;
 
-@SuppressWarnings({ "unused", "null" })
+
 public abstract class BaseMachineRecipeBuilder<T extends BaseMachineRecipeBuilder<T>> extends BaseRecipeBuilder
         implements ItemAttach.Input.ItemCounted<T>, ItemAttach.Output.SimpleOutputItem<T> {
 
@@ -130,7 +109,7 @@ public abstract class BaseMachineRecipeBuilder<T extends BaseMachineRecipeBuilde
         return getBuilder();
     }
 
-    @SuppressWarnings("deprecation")
+    @SuppressWarnings({ "deprecation", "null" })
     @Override
     public Identifier getSuffix(String extra) {
 
